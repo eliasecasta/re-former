f717ec4c1553f3d37a7831f9e2e8ef20a7e5401a
s0kil <dtechtours@gmail.com>
2020-09-18 18:50:50 -0400

    Added Form Editing Functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
+  def index
+    @users = User.all
+  end
 
   def create
     @user = User.new(user_params)
@@ -11,6 +14,20 @@ class UsersController < ApplicationController
       redirect_to new_users_path
     else
       render :new
+    end
+  end
+
+  def edit
+    console
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render 'edit'
     end
   end
 
